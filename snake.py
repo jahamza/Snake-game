27120661606201eb1ac387dd8a8e355d0f463a24
jahamza <58932106+jahamza@users.noreply.github.com>
2019-12-17 13:33:38 +0200

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 
 window = turtle.Screen()
@@ -11,6 +12,14 @@ snake = turtle.Turtle()
 snake.shape("square")
 snake.color("white")
 snake.penup()
+snake.goto(-200, 0)
+
+
+food = turtle.Turtle()
+food.shape("square")
+food.color("gray")
+food.penup()
+food.goto(0, 0)
 
 # while True:
 #     snake.forward(speed)
@@ -24,12 +33,30 @@ def snake_left():
     snake.setheading(180)
     while True:
         snake.forward(speed)
+
+def snake_down():
+    snake.setheading(270)
+    while True:
+        snake.forward(speed)
+
+def snake_right():
+    snake.setheading(360)
+    while True:
+        snake.forward(speed)
     
 
 window.listen()
 window.onkeypress(snake_up, "Up")
 window.onkeypress(snake_left, "Left")
+window.onkeypress(snake_down, "Down")
+window.onkeypress(snake_right, "Right")
 
 
+
+
+if snake.distance(food) < 20:
+    x = random.randint(-550, 550)
+    y = random.randint(-550, 550)
+    food.goto(x,y)
 
 delay = input("Press Enter to finish!")
